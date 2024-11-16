ec58762c62e06668ada3aa696df857460862ae74
sujin <csujin2040@gmail.com>
2024-11-16 14:23:38 +0900

    Fix : 콘솔 찍히는 거 삭제

--- src/components/calendar/WeekCalendar/WeekCalendar.jsx
+++ src/components/calendar/WeekCalendar/WeekCalendar.jsx
@@ -17,8 +17,7 @@ const WeekCalendar = ({ username, currentDate }) => {
       .then((response) => {
         setPlans(response.data.result || {});
         setLoading(false);
-        console.log(response.data.result);
-        console.log(currentDate);
+  
       })
       .catch((error) => {
         console.error(error);
